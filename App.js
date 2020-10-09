6c3fbdbf651f4b70e21784fac1f3122690a501fd
difuentes <fuentesdiego054@gmail.com>
2020-10-09 15:55:14 -0300

    Inicio de proyecto , Reporte Climatico Consumo de API

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import {
+ 
+  View,
+  Text,
+} from 'react-native';
+
+
+const App  = () => {
+  return (
+    <>
+      <Text>DiFuentes</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
